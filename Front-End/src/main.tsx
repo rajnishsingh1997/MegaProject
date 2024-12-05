985d6b14cc1084e77424816d927080c2be2f1bf8
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2024-12-05 10:16:57 +0530

    Added sidebar navigation on home page

--- Front-End/src/main.tsx
+++ Front-End/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "./components/AppSidebar.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <SidebarProvider>
+    <AppSidebar/>
+      <SidebarTrigger />
+      <App />
+    </SidebarProvider>
+  </StrictMode>
+);
