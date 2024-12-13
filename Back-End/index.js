627d2e0c39aa8b52013a10ef39695a5a2d91f23d
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2024-12-13 18:39:36 +0530

    Added sign-up route

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,13 +1,45 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import dotenv from "dotenv";
-import  connectToDatabase  from "./config/db.js";
+import bcrypt from "bcrypt";
+import connectToDatabase from "./config/db.js";
+import User from "./models/UserSchema.js";
 
 dotenv.config();
-
 const app = express();
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+connectToDatabase();
 
-connectToDatabase()
+app.get("/sign-up", async (req, res, next) => {
+  const { name, email, password } = req.body;
+  if(!name || !email || !password) {
+    res.send({
+        message:"Please provide all the details",
+    })
+  }
+  let user = await User.findOne({ email: req.body.email });
+  if (user) {
+    res.send({
+      message: "You already have account, Please login",
+    });
+  } else {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      const user = new User({
+        name: name,
+        password: hashedPassword,
+        email: email,
+      });
+      await user.save();
+      return res.status(201).json(user);
+    } catch (err) {
+      return res.status(400).json({ message: err.message });
+    }
+  }
+});
 
+app.listen(process.env.PORT, () =>
+  console.log(`Server Started on port ${process.env.PORT}`)
+);
